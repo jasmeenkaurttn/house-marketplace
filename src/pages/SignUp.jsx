18f4032eb8dde062ca3a8f5e62d5e18ce2e2708c
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2022-03-14 16:56:27 +0530

    Register user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {setdoc, doc, serverTimestamp} from'firebase/firestore'
+import {db} from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { setDoc } from 'firebase/firestore'
 
 function SignUp() {
     const [showPassword, setShowPassword] = useState(false)
@@ -13,13 +17,39 @@ function SignUp() {
 
     const { name, email, password } = formData
 
-    const naviagte = useNavigate()
+    const navigate = useNavigate()
 
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
             [e.target.id]: e.target.value,
         }))
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth() 
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredential.user // we are getting user from that userCredential
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            const formDataCopy = {...formData} // copying everything which is in formData -> name, email & pwd
+            delete formDataCopy.password // deleting pwd -> bcse we don't want to put it in db
+            formDataCopy.timestamp = serverTimestamp() // setting TS to server's TS
+
+            await setDoc(doc(db, 'users', user.uid), formDataCopy) // update the db & add our user to the user's collection
+
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <>
@@ -30,7 +60,7 @@ function SignUp() {
                     </p>
                 </header>
 
-                <form>
+                <form onSubmit={onSubmit}>
                     <input type="text" className="nameInput" placeholder='Name' id='name' value={name} onChange={onChange} />
                     <input type="email" className="emailInput" placeholder='Email' id='email' value={email} onChange={onChange} />
 
