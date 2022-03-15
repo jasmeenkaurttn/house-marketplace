c6d3d38d294ea60488f8b15f5ccaf478ea23ec33
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2022-03-15 20:53:14 +0530

    Alerts with react toastify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import {setdoc, doc, serverTimestamp} from'firebase/firestore'
 import {db} from '../firebase.config'
@@ -48,7 +49,7 @@ function SignUp() {
 
             navigate('/')
         } catch (error) {
-            console.log(error)
+            toast.error('Something went wrong with registration')
         }
     }
     return (
