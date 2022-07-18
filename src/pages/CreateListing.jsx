ceee84d7a9b1f4674b70975ebfea3d99315c4d88
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2022-07-18 20:46:52 +0530

    Save listing to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
@@ -50,7 +51,7 @@ function CreateListing() {
   }, [isMounted])
 
 
-  const onSubmit = e => {
+  const onSubmit = async(e) => {
     e.preventDefault()
 
     setLoading(true)
@@ -112,7 +113,7 @@ function CreateListing() {
       })
     }
 
-    const imageUrls =  Promise.all(
+    const imageUrls =  await Promise.all(
       [...images].map((image) => storeImage(image))
     ).catch(() => {
       setLoading(false)
@@ -120,9 +121,25 @@ function CreateListing() {
       return
     })
 
-    console.log(imageUrls);
+    const formDataCopy = {
+      ...formData,
+      imageUrls,
+      geolocation,
+      timestamp: serverTimestamp()
+    }
 
+    delete formDataCopy.images
+    delete formDataCopy.address
+    // add location var to formDataCopy object
+
+    location && (formDataCopy.location = location)
+    !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+    const docRef = await addDoc(collection(db, 'listings'),
+    formDataCopy)
     setLoading(false)
+    toast.success('Listing saved')
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`)
   }
   const onMutate = e => {
     let boolean = null
